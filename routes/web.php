0eddc597f21b86385934451129d898c75c5be93e
nandofree2 <nandofree2@gmail.com>
2025-10-16 09:07:34 +0700

    add feature user and role

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
-
 Route::resource('books', BookController::class);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/send-code', [AuthController::class, 'sendVerificationCode'])->name('send.code');
+Route::post('/verify', [AuthController::class, 'verify'])->name('verify');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
